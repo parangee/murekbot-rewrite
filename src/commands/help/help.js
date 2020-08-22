b091ddbfa4aa486deddfa97d036c45e585e32e48
PIKOSTUDIO <68010770+parangee@users.noreply.github.com>
2020-08-22 21:38:07 +0900

    Update help.js

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -7,37 +7,13 @@ module.exports = class Help extends Command {
             aliases: ['help', '도움'],
             memberName: 'help',
             group: 'general',
-            description: '도움말',
-            args: [
-                {
-                    type: 'string',
-                    prompt: '도움말을 볼 명령어를 입력하세요',
-                    key: 'command',
-                    default: ''
-                }
-            ]
+            description: '도움말'
         });
     }
 
     async run(msg, args, fromPattern, result) {
         const embed = msg.createEmbed()
-        if (!args.command) {
-            embed.setTitle('도움말')
-            msg.client.registry.groups.forEach(group => {
-                embed.addField(group.name, '`' + group.commands.map(r => r.name).join('` `') + '`')
-            })
-        } else {
-            if (msg.client.registry.commands.get(args.command)) {
-                const cmd = msg.client.registry.commands.get(args.command)
-                embed.setTitle((`도움말 - ${cmd.name}`))
-                embed.setDescription(`\`${cmd.group.name}\` ${
-                        cmd.aliases.length !== 0 && '`' + cmd.aliases.join('` `') + '`'
-                    }\n`)
-                embed.addField('설명', `\`\`\`fix\n${cmd.description}\`\`\``)
-            } else {
-                embed.setTitle('해당하는 명령어가 없어요!')
-            }
-        }
+        embed.setDescription('[여기](https://docs.dosbot.tk)에서 도움말을 확인하세요')
         return msg.say(embed)
     }
 }
